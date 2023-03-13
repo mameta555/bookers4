2c9d79b08298e14d1456a67253d52b028bb42aaa
EC2 Default User <entertainment.omc1@gmail.com>
2023-03-13 12:36:33 +0000

    ユーザーページ作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @books = @user.books
+    @book = Book.new
+  end
+
+  def edit
+  end
+
+  def index
+  end
+end
