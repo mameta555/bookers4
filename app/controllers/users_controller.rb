1e24e2f2c0fb9e6ec5eeda292b7324e93a6f245e
EC2 Default User <entertainment.omc1@gmail.com>
2023-03-30 13:13:39 +0000

    ユーザー名編集ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   def index
     @users = User.all
     @user = current_user
     @book = Book.new
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :edit, :show, :create, :update, :destroy]
   
   devise_for :users
-  resources :users, only: [:show, :edit, :index]
+  resources :users, only: [:show, :edit, :update, :index]
   root to: "homes#top"
   get '/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
